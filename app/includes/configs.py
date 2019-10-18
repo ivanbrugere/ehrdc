5b829a763ca0236fd322850e2ebbb7e1b111864f
Ivan Brugere <ibrugere@salesforce.com>
2019-10-17 20:56:34 -0700

    changed CV experiment for submit

--- app/includes/configs.py
+++ app/includes/configs.py
@@ -31,23 +31,21 @@ def get_naivebayes_baseline_config():
 
 def get_baseline_cv_configs():
     configs = dict()
-    #configs["gb"] = get_base_config()
+    configs["gb"] = get_base_config()
     #configs["knn-25"] = get_base_config(model_fn=KNeighborsClassifier,
     #                                                  model_params={"n_neighbors": 25})
     #configs["knn-50"] = get_base_config(model_fn=KNeighborsClassifier,
     #                                                  model_params={"n_neighbors": 50})
     #configs["knn-100"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 100})
     #configs["knn-200"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 200})
-    configs["knn-300"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 300})
-    configs["knn-500"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 500})
-    configs["knn-1000"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 1000})
-    configs["knn-2000"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 2000})
-    #configs["nb"] = get_base_config(model_fn=BernoulliNB)
-    configs["nb-Compliment"] = get_base_config(model_fn=ComplementNB)
+    #configs["knn-300"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 300})
+    #configs["knn-500"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 500})
+    #configs["knn-1000"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 1000})
+    #configs["knn-2000"] = get_base_config(model_fn=KNeighborsClassifier, model_params={"n_neighbors": 2000})
+    configs["nb"] = get_base_config(model_fn=BernoulliNB)
+    #configs["nb-Compliment"] = get_base_config(model_fn=ComplementNB)
     #configs["nb-Multi"] = get_base_config(model_fn=MultinomialNB)
-    #configs["random stratified"] = get_base_config(model_fn=DummyClassifier,
-    #                                                             model_params={"strategy": "stratified"})
-    #configs["random uniform"] = get_base_config(model_fn=DummyClassifier,
-    #                                                          model_params={"strategy": "uniform"})
+    configs["random stratified"] = get_base_config(model_fn=DummyClassifier,model_params={"strategy": "stratified"})
+    configs["random uniform"] = get_base_config(model_fn=DummyClassifier,model_params={"strategy": "uniform"})
     configs["rf"] = get_rf_baseline_config()
     return configs
